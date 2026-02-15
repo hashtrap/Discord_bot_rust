14c7288e8ae90953e4acee1973e455fd16dca185
hashtrap <127797448+hashtrap@users.noreply.github.com>
2026-02-15 18:50:35 +0200

    Removed duplicate slash command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use poise::serenity_prelude as serenity;
-use serenity::model::id::GuildId;
+use serenity::model::id::{GuildId,CommandId};
 use dotenv;
+use serenity::all::ApplicationId;
 
 struct Data{}
 type Error=Box<dyn std::error::Error+Send+Sync>;
@@ -8,18 +9,42 @@ type Context<'a>=poise::Context<'a,Data,Error>;
 
 //Below are the extra functions that are used because i like to
 
-fn prepare_env(){
+fn prepare_env()
+{
 
     dotenv::from_filename(".env").ok();
     println!("Preparing environment variables...");
 
 }
 
+//Use only for testing or to delete a global command created by mistake
+fn clear_global_command(token:String,app_id:String)
+{
+    let id:u64=app_id.parse::<u64>().expect("You forgot to the integer");
+    let http=serenity::http::Http::new(&token);
+    http.set_application_id(ApplicationId::new(id));   //1472625721297080471   1472322847161585765
+
+
+    let result=http.delete_global_command(CommandId::new(1472322847161585765)).await;
+
+
+    if result.is_ok()
+    {
+        println!("command erasure done");
+    }
+
+    else
+    {
+        println!("command erasure not done {}",result.err().unwrap());
+    }
+}
+
 //Below are located all the disocrd/poise framework related functions
 
 #[poise::command(slash_command)]
 
-async fn hello(ctx:Context<'_>) -> Result<(),Error>{
+async fn hello(ctx:Context<'_>) -> Result<(),Error>
+{
 
     let response="Hello im a simple bot, serving as this motels bell hop";
     ctx.say(response).await?;
@@ -27,7 +52,8 @@ async fn hello(ctx:Context<'_>) -> Result<(),Error>{
 }
 
 #[poise::command(slash_command)]
-async fn duet(ctx:Context<'_>)->Result<(),Error>{
+async fn duet(ctx:Context<'_>)->Result<(),Error>
+{
 
     let response="Duet works";
     ctx.say(response).await?;
@@ -41,20 +67,25 @@ async fn main()
     prepare_env();
 
     let token= std::env::var("DISCORD_TOKEN").expect("You forgot the fucking token you moron");
+
+    let app_id= std::env::var("APPLICATION_ID").expect("You forgot the fucking app_id you moron");
+
     let intents=serenity::GatewayIntents::non_privileged();
 
     let framework= poise::Framework::builder()
-        .options(poise::FrameworkOptions {
-            commands:vec![hello()
-                          ,duet()],
+        .options(poise::FrameworkOptions
+        {
+            commands:vec![duet(),hello()],
             ..Default::default()
         })
-        .setup(|ctx, _ready, framework|{
-            Box::pin(async move{
+        .setup(|ctx, _ready, framework|
+            {
+            Box::pin(async move
+                {
                 poise::builtins::register_in_guild(ctx, &framework.options().commands,GuildId::new(1470487901958574214)).await?;
                 Ok(Data {})
+                })
             })
-        })
         .build();
 
     let client=serenity::ClientBuilder::new(token, intents)
